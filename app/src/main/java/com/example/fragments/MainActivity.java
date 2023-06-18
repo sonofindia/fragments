7de92cc8531fa4c736e9bbdeace910c005798c98
sonofindia <swapson751@gmail.com>
2023-06-18 12:35:10 +0530

    ok

---
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.fragments;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// get the reference of Button's
+        firstFragment = (Button) findViewById(R.id.firstFragment1);
+        secondFragment = (Button) findViewById(R.id.secondFragment2);
+
+// perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// load First Fragment
+                loadFragment(new FragmentOne());
+            }
+        });
+// perform setOnClickListener event on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// load Second Fragment
+                loadFragment(new FragmentTwo());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment) {
+// create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+// replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout1, fragment);
+        fragmentTransaction.commit(); // save the changes
+    }
+}
